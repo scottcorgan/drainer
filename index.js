c0146bce9830f0ab6bb83f9bf69675285fdecab3
Scott Corgan <scottcorgan@gmail.com>
2014-01-27 16:57:03 -0800

    Support passing in default arguments to pass to each method

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
-var drainer = function(queue, callback, args) {
+var asArray = require('as-array');
+
+var drainer = function(queue) {
+  return function () {
+    var defaultArgs = asArray(arguments);
+    var callback = defaultArgs.pop();
+    
+    drain(queue, [], callback, defaultArgs);
+  };
+};
+
+var drain = function (queue, args, callback, defaultArgs) {
   var fn = queue.shift();
+  args = args ? defaultArgs.concat(args) : defaultArgs;
   
   if (!fn) return callback.apply(callback, [null].concat(args));
   
-  args = args || [];
+  // Add the queue method callback to the args list
   args.push(function () {
     var passedArgs = [].slice.call(arguments);
     var err = passedArgs.shift();
     
     if (err) return callback(err);
     
-    drainer(queue, callback, passedArgs);
+    drain(queue, passedArgs, callback, defaultArgs);
   });
   
+  // Call the queue method
   fn.apply(fn, args);
 };
 
